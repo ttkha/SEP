66f029ea09da7fdd222ead54c041b0f105b86af9
KhaTran <ttkha113@gmail.com>
2018-05-03 16:23:16 +0700

    Login

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        SEPEntities db = new SEPEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var user = db.Users.FirstOrDefault(x => x.UserName == username);
+            if (user != null)
+            {
+                if (user.Password.Equals(password))
+                {
+                    Session["ID"] = user.ID;
+                    //Session["MaGV"] = user.maGV;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ViewBag.mgs = "tai khoang khong ton tai";
+            }
+            return View();
+        }
+
+
+    }
+}
