5df1cebf4c582df7f3488a60f72405b02361f41a
Kha Tran <ttkha113@gmail.com>
2018-06-01 01:20:44 +0700

    hiển thị môn học, khóa học, Sv

---
+++ WebApplication1/Controllers/CourseController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CourseController : Controller
+    {
+        SEP_DTBEntities1 db = new SEP_DTBEntities1();
+        // GET: Student
+        public ActionResult Index(string id)
+        {
+            int maMH = int.Parse(id);
+            var model = db.Courses.Where(x => x.MaMH == maMH);
+            return View(model);
+        }
+
+        public ActionResult ListStudent(string id)
+        {
+            int ID = int.Parse(id);
+            var model = db.ThamDus.Where(x => x.ID_Course == ID);
+            ViewBag.ID = ID;
+            return View(model);
+        }
+
+        public ActionResult ListBuoi(string id)
+        {
+            int ID = int.Parse(id);
+            var model = db.BuoiHocs.Where(x => x.ID_Course == ID);
+            return View(model);
+        }
+
+        public ActionResult ListDiemDanh(string id)
+        {
+            int ID = int.Parse(id);
+            var model = db.DiemDanhs.Where(x => x.ID_BuoiHoc==ID);
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/MonHocController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class MonHocController : Controller
+    {
+        SEP_DTBEntities1 db = new SEP_DTBEntities1();
+        // GET: MonHoc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Open()
+        {
+            int ID = (int)Session["ID"];
+            var model = db.GiangDays.Where(x => x.Status == true && x.ID_GiangVien == ID);
+            return View(model);
+        }
+
+        public ActionResult Close()
+        {
+            int ID = (int)Session["ID"];
+            var model = db.GiangDays.Where(x => x.Status == false && x.ID_GiangVien == ID);
+            return View(model);
+        }
+    }
+}
