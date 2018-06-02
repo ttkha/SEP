912adca0e777eadc99bf0d9819b68cf28ceac79f
Kha Tran <ttkha113@gmail.com>
2018-06-02 21:41:11 +0700

    Change Project

---
+++ WebApplication2/WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        SEP_DTBEntities1 db = new SEP_DTBEntities1();
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var user = db.GiangViens.FirstOrDefault(x => x.Username == username);
+            if (user != null)
+            {
+                if (user.Password.Equals(password))
+                {
+                    Session["ID"] = user.ID;
+                    Session["name"] = user.TenGV;
+                    Session["monhoc"] = user.GiangDays.FirstOrDefault(x => x.ID_GiangVien == user.ID).ID_MonHoc;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult LogOut()
+        {
+            Session["ID"] = null;
+            Session["name"] = null;
+            Session["monhoc"] = null;
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
