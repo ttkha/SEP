d215885edcf5225224f69f40f730443fe8e75277
Kha Tran <ttkha113@gmail.com>
2018-06-13 21:24:00 +0700

    xu Li API KHoa HOc, Mon Hoc

---
+++ WebApplication2/WebApplication2/DAL/Data.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+using WebApplication2.DAL;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication2.DAL
+{
+    public class Data
+    {
+        SepEntities db = new SepEntities();
+        Utility ut = new Utility();
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        SqlCommandBuilder scd;
+        static string strConection = "data source=DESKTOP-CNDGSBR\\SQLEXPRESS;initial catalog=Sep;user id=sa;password=123456;MultipleActiveResultSets=True;";
+        SqlConnection conn = new SqlConnection(strConection);
+        public async Task<string> InsertLesson(MonHoc lesson)
+        {
+            try
+            {
+                db.MonHocs.Add(lesson);
+                await db.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return "";
+
+
+
+        }
+        public async Task<string> InsertCourse(KhoaHoc course)
+        {
+
+            try
+            {
+
+                db.KhoaHocs.Add(course);
+                await db.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return "";
+
+        }
+        public int checkLesson(string id)
+        {
+            conn = ut.OpenDb();
+            conn.Open();
+            cmd = new SqlCommand("checkLesson", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@MAMH", SqlDbType.NChar).Value = id.Trim();
+            cmd.Parameters.Add("@res", System.Data.SqlDbType.Int).Direction = System.Data.ParameterDirection.ReturnValue;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            int retval = (int)cmd.Parameters["@res"].Value;
+            return retval;
+        }
+        public int checkCourse(string id)
+        {
+            conn = ut.OpenDb();
+            conn.Open();
+            cmd = new SqlCommand("checkCourse", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@MAKH", SqlDbType.NChar).Value = id.Trim();
+            cmd.Parameters.Add("@res", System.Data.SqlDbType.Int).Direction = System.Data.ParameterDirection.ReturnValue;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            int retval = (int)cmd.Parameters["@res"].Value;
+            return retval;
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/DAL/Utility.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.DAL
+{
+    class Utility
+    {
+        #region Project
+        public static SqlConnection conn;
+        public static SqlCommand cmd;
+        public static SqlDataAdapter da;
+
+        static string strConection = "data source=DESKTOP-CNDGSBR\\SQLEXPRESS;initial catalog=Sep;user id=sa;password=123456;MultipleActiveResultSets=True;";
+
+        public SqlConnection OpenDb()
+        {
+            conn = new SqlConnection(strConection);
+            return conn;
+        }
+        public static void OpenConnection()
+        {
+            // sever name
+            // chuoi xac thuc
+            try
+            {
+                // mo ket noi
+                conn = new SqlConnection(strConection);
+                conn.Open();
+            }
+            catch (SqlException ex)
+            {
+
+            }
+        }
+        // dong vs ngat ket noi voi database
+        public static void DisConnection()
+        {
+            // dong ket noi
+            conn.Close();
+            conn.Dispose();
+            conn = null;
+        }
+        // Tao bang de luu co sao du lieu
+        public static DataTable getDataTable(string sql)
+        {
+            // khoi tao 1 sqlcommand de tro toi du lieu trogn database
+            cmd = new SqlCommand(sql, conn);
+            // khoi tao 1 sqladapter de luu du lieu tu tren database
+            da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+
+            // tao 1 datatable de hien thi du lieu
+            DataTable table = new DataTable();
+            da.Fill(table);
+            da.Dispose();
+            return table;
+        }
+        // Tao ham Excute de co the tao tac voi co so du lieu
+        public void Excute(string sql)
+        {
+            cmd = new SqlCommand(sql, conn);
+            // goi ham ExcuteNonQuery de co the thuc hien cac thao tac Delete, Insert, Update cho database
+            cmd.ExecuteNonQuery();
+        }
+        #endregion
+    }
+}
